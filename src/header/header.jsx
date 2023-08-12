7fe9ba8fbc669de23bb3eecebd0910cb42882a73
Gabriel <gabrielpablobarragan@gmail.com>
2023-08-12 16:22:16 -0700

    fixes

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -7,8 +7,6 @@ export function Header() {
     return (
         <div className="header">
             <img src={logo} alt='basic-lending-logo' />
-            <div>4805225182</div>
-            <div>info@basislending.com</div>
             <button>Apply Now</button>
         </div>
     );
