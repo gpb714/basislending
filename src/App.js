5656dafd71a38b26abb387852a2e9d7defd75ab3
Gabriel <gabrielpablobarragan@gmail.com>
2023-08-24 08:24:07 -0700

    About section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import { About } from './about/About';
 import { Header } from './header/header';
 
 function App() {
   return (
-    <Header />
+    <>    
+      <Header />
+      <About />
+    </>
   );
 }
 
---
+++ src/about/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './about.css';
+
+export function About() {
+    return (
+        <div id="About" className="about">
+            <div className="left-align">
+                <div id="top-text">
+                    <span>Financing a home is one </span><br /><span>of the biggest decisions of a life time. </span>
+                </div>
+                <div id="bottom-text">
+                    <span>Let our expertise help you make the best one possible.</span>
+                </div>
+            </div>
+            <div id="green-text">
+			    <span>Already Know What You Need?</span>
+                <br />
+		    </div>
+            <div id="white-text">
+			    <span>How We Can Help</span>
+                <br />
+		    </div>
+	    </div>
+    );
+}
