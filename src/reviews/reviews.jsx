939f5860bbaa97bc48fe0632889754ec0488e96e
Gabriel <gabrielpablobarragan@gmail.com>
2023-09-04 13:45:06 -0700

    cleanup

--- src/reviews/reviews.jsx
+++ src/reviews/reviews.jsx
@@ -4,7 +4,7 @@ import './reviews.css';
 
 export function Reviews() {
     return (
-        <div className='padding-top'>
+        <div className='padding'>
             <iframe src='https://widget-f1afd54d767d4a88b99c5de4c7ffdf20.elfsig.ht' width='100%' height='450px' frameborder='0'></iframe>
         </div>
     );
