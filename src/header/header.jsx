094acf02794a22efbb28ed83e2ec441ad7dddcf4
Gabriel <gabrielpablobarragan@gmail.com>
2023-09-04 12:22:22 -0700

    naviagtion on click apply now

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -4,10 +4,15 @@ import logo from './basislending.png';
 import './header.css';
 
 export function Header() {
+
+    function navigateToApp(){
+        window.location.href = 'https://basislending.my1003app.com/242392/register';
+    }
+
     return (
         <div className="header">
             <img src={logo} alt='basic-lending-logo' />
-            <button>Apply Now</button>
+            <button onClick={navigateToApp}>Apply Now</button>
         </div>
     );
 }
