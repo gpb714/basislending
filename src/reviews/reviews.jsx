0f021bbff3b78496ee59bc7e5acaa6b7dabd3ac4
Gabriel <gabrielpablobarragan@gmail.com>
2023-09-27 12:27:26 -0700

    added reviews widget

--- src/reviews/reviews.jsx
+++ src/reviews/reviews.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Helmet } from "react-helmet";
 
 import './reviews.css';
 
 export function Reviews() {
     return (
         <div className='padding'>
-            <iframe src='https://widget-f1afd54d767d4a88b99c5de4c7ffdf20.elfsig.ht' width='100%' height='450px' frameborder='0'></iframe>
+            <div id='ss-custom-reviews-widget-root'></div>  
+            <Helmet>            
+                <script id='ss-custom-reviews-widget-script' src='https://d1gxt2ovmgw1zu.cloudfront.net/custom_reviews_widget_script.js' ss-custom-reviews-widget-api-key='BLp_UN26bq5G5KF8JzuAZ5LsBHDU-aztupzR6gGKdrO9sq_kLO5iXN44KafxhKoC9vH6fXuK8SvM7Hdc' widget-key='ieOlXoKVHxvIlGJqf2qq3Q' type='text/javascript'> </script>
+            </Helmet>
         </div>
     );
 }
